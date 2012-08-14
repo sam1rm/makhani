89053e078203c8c234ae49d8f32e4a455f96c5bf
Samir Makhani <makhani@berkeley.edu>
2012-08-14 16:26:25 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makhani::Application.config.secret_token = 'd5685e7947331d9a610f185ef48f628e8412f15fff6cba9ddce436a824289dd5001098e4a70fc40452bc95284eaa82b9e40c692bb6bd1461a7d787363ddacbbd'
